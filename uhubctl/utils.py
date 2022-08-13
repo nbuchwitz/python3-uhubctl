3bb4d4ea92109b3770fc858be145c83921902afc
Nicolai Buchwitz <nb@tipi-net.de>
2022-08-13 17:58:22 +0200

    Split functionalily into separate files
    Signed-off-by: Nicolai Buchwitz <nb@tipi-net.de>

---
+++ uhubctl/utils.py
@@ -0,0 +1,25 @@
+"""Utilities around uhubctl binary"""
+import subprocess
+
+UHUBCTL_BINARY = "uhubctl"
+
+
+def _uhubctl(args: list = None) -> list:
+    cmd = UHUBCTL_BINARY.split(" ")
+    cmd.append('-N')
+
+    if args is not None:
+        cmd += args
+
+    try:
+        result = subprocess.run(cmd, capture_output=True, check=True)
+        stdout = result.stdout.decode()
+
+        return stdout.split('\n')
+    except subprocess.CalledProcessError as exc:
+        stderr = exc.stderr.decode()
+
+        if stderr.startswith("No compatible devices detected"):
+            return []
+
+        raise Exception(f"uhubctl failed: {stderr}") from exc
