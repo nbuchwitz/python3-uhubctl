de2227e2cb6f1d7d5aa4432e232f90417bbd8412
Nicolai Buchwitz <nb@tipi-net.de>
2022-12-28 16:49:50 +0100

    chore: change packaging to setuptools
    Change packaging to setuptools in order to support pip install from
    github
    
    Signed-off-by: Nicolai Buchwitz <nb@tipi-net.de>

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="uhubctl",
+    version="0.1.2",
+    author="Nicolai Buchwitz",
+    author_email="nb@tipi-net.de",
+    description="A basic Python wrapper for uhubctl",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/nbuchwitz/python3-uhubctl",
+    packages=["uhubctl"],
+    python_requires=">=3.5",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/nbuchwitz/python3-uhubctl/issues"
+    },
+)
